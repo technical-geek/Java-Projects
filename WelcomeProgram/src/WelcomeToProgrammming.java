04b747bb9775878961b77733f4ec966d50064b50
Chaitanya Nukala <chaitanya.nukala@personalcapital.com>
2019-02-15 09:55:56 -0800

    added function to print array

--- WelcomeProgram/src/WelcomeToProgrammming.java
+++ WelcomeProgram/src/WelcomeToProgrammming.java
@@ -1,2 +1,13 @@
 public class WelcomeToProgrammming {
+    public static void main (String[] args){
+        printArray();
+
+    }
+    static void printArray (){
+        int num [] = {2,3,4,5,6,7,8,1,6};
+        for (int i =0; i<num.length;i++){
+            System.out.println(num[i]);
+
+        }
+    }
 }
